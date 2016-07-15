dc063513e618d627b8fbdd3e4afb3097ca8bae6c
Byeonggon Lee <gonny952@gmail.com>
2016-07-15 17:28:03 +0900

    Modify gonc_string.h

--- src/gonc_string.h
+++ src/gonc_string.h
@@ -1,6 +1,8 @@
 #ifndef _GONC_STRING_H
 #define _GONC_STRING_H
 
+#include <string.h>
+
 struct gonc_string
 {
     char* value;
@@ -13,12 +15,20 @@ struct gonc_string
 #define GONC_STRING_GET_LENGTH(string) \
 (string)->length
 
-#define GONC_STRING_SET(string, _value, _length) \
-do                                               \
-{                                                \
-    (string)->value = _value;                    \
-    (string)->length = _length;                  \
-}                                                \
+#define GONC_STRING_SET(string, _value)         \
+do                                              \
+{                                               \
+    (string)->value = _value;                   \
+    (string)->length = strlen((string)->value); \
+}                                               \
+while(0)
+
+#define GONC_STRING_SET2(string, _value, _length) \
+do                                                \
+{                                                 \
+    (string)->value = _value;                     \
+    (string)->length = _length;                   \
+}                                                 \
 while(0)
 
 #endif
