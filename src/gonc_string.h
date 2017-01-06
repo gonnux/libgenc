23f4fb58c6401dae001b6fd32377cfdaa7798071
Byeonggon Lee <gonny952@gmail.com>
2017-01-06 16:32:48 +0900

    Update gonc_string

--- src/gonc_string.h
+++ src/gonc_string.h
@@ -1,12 +1,14 @@
 #ifndef _GONC_STRING_H
 #define _GONC_STRING_H
 
+#include <stdbool.h>
 #include <string.h>
 
 struct gonc_string
 {
     char* chars;
     size_t length;
+    bool freeable;
 };
 
 #define GONC_STRING_GET_CHARS(string) \
@@ -15,20 +17,25 @@ struct gonc_string
 #define GONC_STRING_GET_LENGTH(string) \
 (string)->length
 
-#define GONC_STRING_SET(string, _chars)         \
-do                                              \
-{                                               \
-    (string)->chars = _chars;                   \
-    (string)->length = strlen((string)->chars); \
-}                                               \
+#define GONC_STRING_IS_FREEABLE(string) \
+(string)->freeable
+
+#define GONC_STRING_SET(string, _chars, _freeable) \
+do                                                 \
+{                                                  \
+    (string)->chars = _chars;                      \
+    (string)->length = strlen((string)->chars);    \
+    (string)->freeable = _freeable;                \
+}                                                  \
 while(0)
 
-#define GONC_STRING_SET2(string, _chars, _length) \
-do                                                \
-{                                                 \
-    (string)->chars = _chars;                     \
-    (string)->length = _length;                   \
-}                                                 \
+#define GONC_STRING_SET2(string, _chars, _length, _freeable) \
+do                                                           \
+{                                                            \
+    (string)->chars = _chars;                                \
+    (string)->length = _length;                              \
+    (string)->freeable = _freeable;                          \
+}                                                            \
 while(0)
 
 #endif
