a428b1ba6c6ccef65e127b57ca7d642a0bb7ac64
Byeonggon Lee <gonny952@gmail.com>
2017-08-09 10:01:04 +0000

    Commit

--- src/genc_uIntToStr.h
+++ src/genc_uIntToStr.h
@@ -1,28 +1,28 @@
-#ifndef _GENC_INT_TO_N_STR
-#define _GENC_INT_TO_N_STR
+#ifndef _GENC_UINT_TO_N_STR
+#define _GENC_UINT_TO_N_STR
 
 #include <stdlib.h>
 #include <stdio.h>
-static inline size_t genc_intToNStr(int _integer, int base, char** string) {
+static inline size_t genc_intToNStr(int _uInteger, int base, char** string) {
     size_t length = 0;
 
     if(base == 10 || base == 16) {
-        int integer = _integer;
+        int uInteger = _uInteger;
         do {
-            integer /= base;
+            uInteger /= base;
             ++length;
         }
-        while(integer > 0);
-        integer = _integer;
+        while(uInteger > 0);
+        uInteger = _uInteger;
 
         *string = malloc((length + 1) * sizeof(char));
         for(size_t index = 0; index != length; ++index) {
-            int remainder = integer % base;
+            int remainder = uInteger % base;
             if(remainder < 10)
                 (*string)[length - index - 1] = '0' + remainder;
             else
                 (*string)[length - index - 1] = 'A' + remainder - 10;
-            integer /= base;
+            uInteger /= base;
         }
     }
     (*string)[length] = '\0';
