996fb4ff5a259a3ed6ca1330ed7dd3e8ebf09622
Byeonggon Lee <gonny952@gmail.com>
2017-08-11 23:45:29 +0000

    Commit

--- src/genc_Array.h
+++ src/genc_Array.h
@@ -8,35 +8,35 @@ struct                   \
 {                        \
     type* elements;      \
     size_t size;         \
-} genc_array
+} genc_Array
 
 #define GENC_ARRAY_INIT(array, _size)                                                       \
 do                                                                                          \
 {                                                                                           \
-    (array)->genc_array.elements = malloc(_size * sizeof(*((array)->genc_array.elements))); \
-    (array)->genc_array.size = _size;                                                       \
+    (array)->genc_Array.elements = malloc(_size * sizeof(*((array)->genc_Array.elements))); \
+    (array)->genc_Array.size = _size;                                                       \
 }                                                                                           \
 while(0)
 
 #define GENC_ARRAY_SIZE(array) \
-(array)->genc_array.size
+(array)->genc_Array.size
 
 #define GENC_ARRAY_GET(array, index) \
-(array)->genc_array.elements[index]
+(array)->genc_Array.elements[index]
 
 #define GENC_ARRAY_SET(array, index, element)          \
 do                                                     \
 {                                                      \
-    if(index < (array)->genc_array.size)               \
-        (array)->genc_array.elements[index] = element; \
+    if(index < (array)->genc_Array.size)               \
+        (array)->genc_Array.elements[index] = element; \
 }                                                      \
 while(0)
 
 #define GENC_ARRAY_FOR_EACH(array, index) \
-for(size_t index = 0; index != (array)->genc_array.size; ++index)
+for(size_t index = 0; index != (array)->genc_Array.size; ++index)
 
 #define GENC_ARRAY_REVERSE_FOR_EACH(array, index) \
-for(size_t index = (array)->genc_array.size - 1; index != -1; --index)
+for(size_t index = (array)->genc_Array.size - 1; index != -1; --index)
 
 #define GENC_ARRAY_SUBSET_FOR_EACH(array, index, start, end) \
 for(size_t index = start; index != end; ++index)
