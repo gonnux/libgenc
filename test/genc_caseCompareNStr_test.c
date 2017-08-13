6fd648fe9a31712aa2a1f8b0f686d8ccc1c77696
Byeonggon Lee <gonny952@gmail.com>
2017-08-13 04:49:08 +0000

    Commit

--- test/genc_caseCompareNStr_test.c
+++ test/genc_caseCompareNStr_test.c
@@ -5,7 +5,16 @@
 
 #include <stdio.h>
 #include "../src/genc_caseCompareNStr.h"
+static void genc_caseCompareNStr_test(void** state) {
+    assert_int_equal(genc_caseCompareNStr("APPLE", sizeof("APPLE") - 1, "apple", sizeof("apple") - 1), 0);
+    assert_int_equal(genc_caseCompareNStr("apple", sizeof("apple") - 1, "APPLE", sizeof("APPLE") - 1), 0);
+    assert_true(genc_caseCompareNStr("apple", sizeof("apple") - 1, "apple!", sizeof("apple!") - 1) < 0);
+    assert_true(genc_caseCompareNStr("banana", sizeof("banana") - 1, "APPLE", sizeof("APPLE") - 1) > 0);
+}
 int main() {
-    printf("%d\n", genc_caseCompareNStr("APPLE", sizeof("APPLE") - 1, "apple", sizeof("apple") - 1));
-    return 0;
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(genc_caseCompareNStr_test),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
 }
