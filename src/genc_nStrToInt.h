c3e2f26b324e2d49d6c937fc87582885eaf8da1d
Byeonggon Lee <gonny952@gmail.com>
2017-08-15 06:04:43 +0000

    genc_nStrToInt_test

--- src/genc_nStrToInt.h
+++ src/genc_nStrToInt.h
@@ -4,14 +4,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 static inline int genc_nStrToInt(char* string, size_t length) {
-    int intValue = 0;
+    int integer = 0;
     int multiplier = 1;
+    bool isNegative = false;
     for(int index = length - 1; index >= 0; --index, multiplier *= 10) {
-        if(string[index] - '0' < 0 || string[index] - '0' > 9)
+        if(index == 0 && (string[0] == '+' || string[0] == '-')) {
+            isNegative = string[0] == '-';
+            break;
+        } else if(string[index] - '0' < 0 || string[index] - '0' > 9)
             return -1;
-        intValue += (string[index] - '0') * multiplier;
+        integer += (string[index] - '0') * multiplier;
     }
-    return intValue;
+    if(isNegative)
+        return integer * -1;
+    return integer;
 }
 
 #endif
