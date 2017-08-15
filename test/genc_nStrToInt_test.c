ee93e25859ed9fd12406adcbbb738a47f4f99df1
Byeonggon Lee <gonny952@gmail.com>
2017-08-15 06:04:43 +0000

    genc_nStrToInt_test

--- test/genc_nStrToInt_test.c
+++ test/genc_nStrToInt_test.c
@@ -1,12 +1,22 @@
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <setjmp.h>
 #include <cmocka.h>
 
 #include <stdio.h>
 #include "../src/genc_nStrToInt.h"
-int main()
-{
-    printf("%d\n", genc_nStrToInt("1234", sizeof("1234") - 1));
-    return 0;
+
+static void genc_nStrToInt_test(void** state) {
+    assert_int_equal(genc_nStrToInt("1024", sizeof("1024") - 1), 1024);
+    assert_int_equal(genc_nStrToInt("+12345", sizeof("+12345") - 1), 12345);
+    assert_int_equal(genc_nStrToInt("-9999", sizeof("-9999") - 1), -9999);
+
+}
+int main() {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(genc_nStrToInt_test),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
 }
