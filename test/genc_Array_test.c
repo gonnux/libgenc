cd2a862a5ca909d699671452ef97f76a5874157c
Byeonggon Lee <gonny952@gmail.com>
2017-08-16 11:10:19 +0000

    genc_Array_test

--- test/genc_Array_test.c
+++ test/genc_Array_test.c
@@ -10,14 +10,18 @@ struct integer_array {
     GENC_ARRAY(int);
 };
 
-int main() {
+static void genc_Array_test(void** state) {
     struct integer_array ia;
     GENC_ARRAY_INIT(&ia, 10);
-    for(int i = 0; i < GENC_ARRAY_SIZE(&ia); ++i) {
+    for(int i = 0; i < GENC_ARRAY_SIZE(&ia); ++i)
         GENC_ARRAY_SET(&ia, i, i * 100);
-        printf("element: %d size: %u\n", GENC_ARRAY_GET(&ia, i), GENC_ARRAY_SIZE(&ia));
-    }
-    GENC_ARRAY_FOR_EACH(&ia, i) {
-        printf("%d\n", GENC_ARRAY_GET(&ia, i));
-    }
+    GENC_ARRAY_FOR_EACH(&ia, i)
+        assert_int_equal(GENC_ARRAY_GET(&ia, i), i * 100);
+}
+
+int main() {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(genc_Array_test),
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
 }
