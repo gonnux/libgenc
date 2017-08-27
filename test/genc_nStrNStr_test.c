535fa4efc080359ac23cd776c3ee7c4bf9319c05
Byeonggon Lee <gonny952@gmail.com>
2017-08-27 04:41:24 +0000

    Commit

--- test/genc_nStrNStr_test.c
+++ test/genc_nStrNStr_test.c
@@ -5,9 +5,11 @@
 
 #include <stdio.h>
 #include "../src/genc_nStrNStr.h"
+
 static void genc_nStrNStr_test(void** state) {
     assert_int_equal(genc_nStrNStr("<<APPLE>>", sizeof("<<APPLE>>") - 1, "APPLE", sizeof("APPLE") - 1), 2);
 }
+
 int main() {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(genc_nStrNStr_test),
