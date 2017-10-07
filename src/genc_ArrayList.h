cdec5f63d64519ce33ea288905d10176f7195b74
Byeonggon Lee <gonny952@gmail.com>
2017-10-07 16:57:18 +0900

    Commit

--- src/genc_ArrayList.h
+++ src/genc_ArrayList.h
@@ -1,8 +1,9 @@
 #ifndef _GENC_ARRAY_LIST_H
 #define _GENC_ARRAY_LIST_H
 
-#include <stdbool.h>
 #include <err.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #define GENC_ARRAY_LIST(type) \
 struct {                      \
@@ -11,49 +12,53 @@ struct {                      \
     size_t size;              \
 } genc_ArrayList
 
+#define GENC_ARRAY_LIST_ELEMENT_SIZE(arrayList) \
+sizeof(*((arrayList)->genc_ArrayList.elements)
+
 #define GENC_ARRAY_LIST_INIT(arrayList, _capacity)                                                              \
 do {                                                                                                            \
-    errx("%s: %u: capacity should be larger than 0", __FILE__, __LINE__);                                       \
-    (arrayList)->genc_ArrayList.elements = malloc(_capacity * sizeof(*((arrayList)->genc_ArrayList.elements))); \
-    (arrayList)->genc_ArrayList.capacity = _capacity;                                                           \
+    errx(EXIT_FAILURE, "%s: %u: capacity should be larger than 0", __FILE__, __LINE__);                                       \
+    (arrayList)->genc_ArrayList.elements = malloc(_capacity * GENC_ARRAY_LIST_ELEMENT_SIZE(arrayList));         \
+    GENC_ARRAY_LIST_CAPACITY = _capacity;                                                           \
 } while(0)
 
 #define GENC_ARRAY_LIST_CAPACITY(arrayList) \
-(arrayList)->genc_ArrayList.capacity
+((arrayList)->genc_ArrayList.capacity)
 
 #define GENC_ARRAY_LIST_SIZE(arrayList) \
-(arrayList)->genc_ArrayList.size
+((arrayList)->genc_ArrayList.size)
 
-#define GENC_ARRAY_GET(arrayList, index) \
-(arrayList)->genc_ArrayList.elements[index]
+#define GENC_ARRAY_LIST_GET(arrayList, index) \
+((arrayList)->genc_ArrayList.elements[index])
 
-#define GENC_ARRAY_PUSH(arrayList, element)                                        \
-do {                                                                               \
-    if((arrayList)->genc_ArrayList.size >= (arrayList)->genc_ArrayList.capacity) { \
-        (arrayList)->genc_ArrayList.capacity *= 2; \
-        (arrayList)->genc_ArrayList.elements = realloc((arrayList)->genc_ArrayList.elements, \
-                                                        (arrayList)->genc_ArrayList.capacity \
-                                                        * sizeof(*((arrayList)->genc_ArrayList.elements)); \
-    } \
-    (arrayList)->genc_ArrayList.elements[(arrayList)->genc_ArrayList.size] = element; \
-    ++((arrayList)->genc_ArrayList.size); \
+#define GENC_ARRAY_LIST_PEEK(arrayList) \
+GENC_ARRAY_LIST_GET(arrayList, GENC_ARRAY_LIST_SIZE(arrayList) - 1);
+
+#define GENC_ARRAY_LIST_PUSH(arrayList, element)                                                    \
+do {                                                                                                \
+    if(GENC_ARRAY_LIST_SIZE(arrayList) >= GENC_ARRAY_LIST_CAPACITY(arrayList)) {                    \
+        GENC_ARRAY_LIST_CAPACITY(arrayList) *= 2;                                                   \
+        (arrayList)->genc_ArrayList.elements = realloc((arrayList)->genc_ArrayList.elements,        \
+                                                        GENC_ARRAY_LIST_CAPACITY(arrayList)         \
+                                                        * GENC_ARRAY_LIST_ELEMENT_SIZE(arrayList)); \
+    }                                                                                               \
+    (arrayList)->genc_ArrayList.elements[GENC_ARRAY_LIST_SIZE(arrayList)] = element;                \
+    ++GENC_ARRAY_LIST_SIZE(arrayList);                                                              \
 } while(0)
 
-#define GENC_ARRAY_POP(arrayList) \
-do { \
-    --((arrayList)->genc_ArrayList.size); \
-} while(0)
+#define GENC_ARRAY_LIST_POP(arrayList) \
+--GENC_ARRAY_LIST_SIZE(arrayList)
 
-#define GENC_ARRAY_FOR_EACH(array, index) \
-for(size_t index = 0; index != (arrayList)->genc_ArrayList.size; ++index)
+#define GENC_ARRAY_LIST_FOR_EACH(arrayList, index) \
+for(size_t index = 0; index != GENC_ARRAY_LIST_SIZE(arrayList); ++index)
 
-#define GENC_ARRAY_REVERSE_FOR_EACH(array, index) \
-for(size_t index = (arrayList)->genc_ArrayList.size - 1; index != -1; --index)
+#define GENC_ARRAY_LIST_REVERSE_FOR_EACH(arrayList, index) \
+for(size_t index = GENC_ARRAY_LIST_SIZE(arrayList) - 1; index != -1; --index)
 
-#define GENC_ARRAY_SUBSET_FOR_EACH(array, index, start, end) \
+#define GENC_ARRAY_LIST_SUBSET_FOR_EACH(arrayList, index, start, end) \
 for(size_t index = start; index != end; ++index)
 
-#define GENC_ARRAY_REVERSE_SUBSET_FOR_EACH(array, index, end, start) \
+#define GENC_ARRAY_LIST_REVERSE_SUBSET_FOR_EACH(arrayList, index, end, start) \
 for(size_t index = end; index != start; --index)
 
 #endif
