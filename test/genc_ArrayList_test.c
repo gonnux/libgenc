9d6ab3c259600f9a6fe592f1a6c8f4a001ae773e
Byeonggon Lee <gonny952@gmail.com>
2017-10-07 18:01:22 +0900

    Commit

--- test/genc_ArrayList_test.c
+++ test/genc_ArrayList_test.c
@@ -13,7 +13,7 @@ struct Integer_ArrayList {
     GENC_ARRAY_LIST(struct Integer);
 };
 
-int main() {
+static void genc_ArrayList_test(void** state) {
     struct Integer_ArrayList arrayList;
     GENC_ARRAY_LIST_INIT(&arrayList, 10);
     struct Integer element1;
@@ -29,5 +29,12 @@ int main() {
         printf("%d\n", GENC_ARRAY_LIST_GET(&arrayList, index).value);
     }
     printf("\n");
-    return 0;
+}
+
+int main() {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(genc_ArrayList_test),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
 }
