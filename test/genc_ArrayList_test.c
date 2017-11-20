5e05252368fb9a1a687a1533b06808cfc0927330
Byeonggon Lee <gonny952@gmail.com>
2017-11-20 20:00:57 +0900

    Fix bugs for ArrayList

--- test/genc_ArrayList_test.c
+++ test/genc_ArrayList_test.c
@@ -13,49 +13,59 @@ struct Integer_ArrayList {
     GENC_ARRAY_LIST(struct Integer);
 };
 
-static void genc_ArrayList_test(void** state) {
-    struct Integer_ArrayList arrayList;
-    GENC_ARRAY_LIST_INIT2(&arrayList, 10);
-    struct Integer element1;
-    element1.value = 100;
-    struct Integer element2;
-    element2.value = 200;
-    struct Integer element3;
-
-    GENC_ARRAY_LIST_PUSH(&arrayList, element1);
-    GENC_ARRAY_LIST_PUSH(&arrayList, element2);
-    GENC_ARRAY_LIST_POP(&arrayList, &element3);
-
-    GENC_ARRAY_LIST_FOR_EACH(&arrayList, index) {
-        printf("%d\n", GENC_ARRAY_LIST_GET(&arrayList, index).value);
-    }
-    printf("%d", element3.value);
-    printf("\n");
-}
-
-static void genc_ArrayList_test_insertion(void** state) {
+static void genc_ArrayList_testInsertion(void** state) {
     struct Integer_ArrayList arrayList;
     GENC_ARRAY_LIST_INIT2(&arrayList, 1);
-    struct Integer element1;
-    element1.value = 100;
-    struct Integer element2;
-    element2.value = 200;
-    struct Integer element3;
-    element3.value = 300;
-    GENC_ARRAY_LIST_INSERT(&arrayList, 0, element1);
-    GENC_ARRAY_LIST_INSERT(&arrayList, 0, element2);
-    GENC_ARRAY_LIST_INSERT(&arrayList, 0, element3);
-    GENC_ARRAY_LIST_FOR_EACH(&arrayList, index) {
-        assert_int_equal(GENC_ARRAY_LIST_GET(&arrayList, index).value, (GENC_ARRAY_LIST_SIZE(&arrayList) - index) * 100);
-        printf("%d\n", GENC_ARRAY_LIST_GET(&arrayList, index).value);
+    struct Integer element;
+    int elementCount = 3;
+    for(int i = 0; i < elementCount; ++i) {
+        element.value = i * 100;
+        GENC_ARRAY_LIST_INSERT(&arrayList, 0, element);
     }
-    printf("capacity %d\n", GENC_ARRAY_LIST_CAPACITY(&arrayList));
+    GENC_ARRAY_LIST_FOR_EACH(&arrayList, index) {
+        element = GENC_ARRAY_LIST_GET(&arrayList, index);
+        assert_int_equal(element.value, (GENC_ARRAY_LIST_SIZE(&arrayList) - index - 1) * 100);
+    }
+}
+
+static void genc_ArrayList_testStack(void** state) {
+    struct Integer_ArrayList arrayList;
+    GENC_ARRAY_LIST_INIT(&arrayList);
+    GENC_ARRAY_LIST_REALLOC(&arrayList, 1);
+    struct Integer element;
+    int elementCount = 10;
+    for(int i = 0; i < elementCount; ++i) {
+        element.value = i * 100;
+        GENC_ARRAY_LIST_PUSH(&arrayList, element);
+    }
+    for(int i = 0; i < elementCount; ++i) {
+        GENC_ARRAY_LIST_POP(&arrayList, &element);
+        assert_int_equal(GENC_ARRAY_LIST_SIZE(&arrayList), elementCount - i - 1);
+        assert_int_equal(element.value, 100 * (elementCount - i - 1));
+    }
+}
+
+static void genc_ArrayList_testQueue(void** state) {
+    struct Integer_ArrayList arrayList;
+    GENC_ARRAY_LIST_INIT(&arrayList);
+    GENC_ARRAY_LIST_REALLOC(&arrayList, 1);
+    struct Integer element;
+    int elementCount = 10;
+    for(int i = 0; i < elementCount; ++i) {
+        element.value = i * 100;
+        GENC_ARRAY_LIST_PUSH(&arrayList, element);
+    }
+    for(int i = 0; i < elementCount; ++i) {
+        GENC_ARRAY_LIST_REMOVE(&arrayList, 0, &element);
+        assert_int_equal(GENC_ARRAY_LIST_SIZE(&arrayList), elementCount - i - 1);
+        assert_int_equal(element.value, i * 100);
+    }
 }
 
 int main() {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(genc_ArrayList_test),
-        cmocka_unit_test(genc_ArrayList_test_insertion)
+        cmocka_unit_test(genc_ArrayList_testInsertion),
+        cmocka_unit_test(genc_ArrayList_testStack)
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
