b3e9b1522f4e555e96e8bdc790085d250864efcc
Byeonggon Lee <gonny952@gmail.com>
2018-01-15 17:16:59 +0900

    Commit

--- src/genc_args.h
+++ src/genc_args.h
@@ -4,7 +4,7 @@
 #define GENC_ARGS(...) \
 (void*[]){__VA_ARGS__, NULL}
 
-#define GENC_COUNT_ARGS(args, count) \
+#define GENC_ARGS_COUNT(args, count) \
 for(*count = 0; args[*count] != NULL; ++(*count))
 
 #endif
