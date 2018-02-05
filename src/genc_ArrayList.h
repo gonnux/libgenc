4fd1c618354d7649dfb93955bd5aa18c499cdba7
Byeonggon Lee <gonny952@gmail.com>
2018-02-05 01:47:27 -0500

    Fixed a bug

--- src/genc_ArrayList.h
+++ src/genc_ArrayList.h
@@ -83,11 +83,11 @@ do {                                                      \
     GENC_ARRAY_LIST_REALLOC(arrayList, initialCapacity);  \
 } while(0)
 
-#define GENC_ARRAY_LIST_INIT3(arraylist, initialSize)    \
-do {                                                     \
-    GENC_ARRAY_LIST_INIT2(arrayList, initialSize * 2);   \
-    GENC_ARRAY_LIST_SIZE(arrayList) = initialSize;       \
-    GENC_ARRAY_LIST_ZERO(arrayList, 0, initialSize - 1); \
+#define GENC_ARRAY_LIST_INIT3(arrayList, initialSize)      \
+do {                                                       \
+    GENC_ARRAY_LIST_INIT2(arrayList, (initialSize) * 2);   \
+    GENC_ARRAY_LIST_SIZE(arrayList) = initialSize;         \
+    GENC_ARRAY_LIST_ZERO(arrayList, 0, (initialSize) - 1); \
 } while(0)
 
 
