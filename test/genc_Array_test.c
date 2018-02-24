54a1e666bee410e51a476be5a8becb899d1032e6
Byeonggon Lee <gonny952@gmail.com>
2018-02-24 00:30:52 -0500

    Tests for genc_List fail

--- test/genc_Array_test.c
+++ test/genc_Array_test.c
@@ -6,17 +6,33 @@
 #include <stdio.h>
 #include "../src/genc_Array.h"
 
-struct integer_array {
+struct Integer_Array {
     GENC_ARRAY(int);
 };
 
+static void Integer_Array_toRawArray(struct Integer_Array* array, int** rawArray, int* rawArraySize) {
+    *rawArray = malloc(GENC_ARRAY_ELEMENT_SIZE(array) * GENC_ARRAY_SIZE(array));
+    assert_non_null(rawArray);
+    GENC_ARRAY_FOR_EACH(array, index) {
+        GENC_ARRAY_GET(array, index, &(*rawArray)[index]);
+    }
+    *rawArraySize = GENC_ARRAY_SIZE(array); 
+    // assertion of arraySize greater than zero is currently omitted.
+}
+
 static void genc_Array_test(void** state) {
-    struct integer_array ia;
-    GENC_ARRAY_INIT(&ia, 10);
-    for(int i = 0; i < GENC_ARRAY_SIZE(&ia); ++i)
-        GENC_ARRAY_SET(&ia, i, i * 100);
-    GENC_ARRAY_FOR_EACH(&ia, i)
-        assert_int_equal(GENC_ARRAY_GET(&ia, i), i * 100);
+    struct Integer_Array integerArray;
+    int rawIntegerArray[] = {1, 2, 3, 4, 5};
+    int rawIntegerArraySize = sizeof(rawIntegerArray) / sizeof(int);
+    GENC_ARRAY_INIT(&integerArray, rawIntegerArraySize);
+    for(int i = 0; i < GENC_ARRAY_SIZE(&integerArray); ++i)
+        GENC_ARRAY_SET(&integerArray, i, rawIntegerArray[i]);
+    int* outputRawIntegerArray;
+    int outputRawIntegerArraySize;
+    Integer_Array_toRawArray(&integerArray, &outputRawIntegerArray, &outputRawIntegerArraySize);
+    assert_int_equal(rawIntegerArraySize, outputRawIntegerArraySize);
+    assert_memory_equal(rawIntegerArray, outputRawIntegerArray, outputRawIntegerArraySize);
+    free(outputRawIntegerArray);
 }
 
 int main() {
--- test/genc_List_test.c
+++ test/genc_List_test.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <setjmp.h>
 #include <cmocka.h>
 #include <stdio.h>
@@ -14,65 +15,124 @@ struct Integer_List {
     GENC_LIST(struct Integer);
 };
 
+static void Integer_List_toRawArray(struct Integer_List* integerList, int** rawArray, int* rawArraySize) {
+    *rawArray = malloc(GENC_LIST_SIZE(integerList) * sizeof(int));
+    assert_non_null(rawArray);
+    int index = 0;
+    GENC_LIST_FOR_EACH(integerList, element) {
+        (*rawArray)[index++] = element->value;
+printf("value %d\n", element->value);
+    }
+    *rawArraySize = GENC_LIST_SIZE(integerList); 
+    assert_true(*rawArraySize >= 0);
+    // assertion of arraySize greater than zero is currently omitted.
+}
+
+static void GENC_LIST_INIT_test(void** state) {
+    struct Integer_List integerList; 
+    GENC_LIST_INIT(&integerList);
+    assert_null(GENC_LIST_HEAD(&integerList));
+    assert_null(GENC_LIST_TAIL(&integerList));
+    assert_int_equal(GENC_LIST_SIZE(&integerList), 0);
+}
+
+void GENC_LIST_ELEMENT_INIT_test(void** state) {
+    struct Integer integer;
+    GENC_LIST_ELEMENT_INIT(&integer);
+    assert_null(GENC_LIST_ELEMENT_NEXT(&integer));
+    assert_null(GENC_LIST_ELEMENT_PREVIOUS(&integer));
+}
+
+void GENC_LIST_ELEMENT_PREPEND_test(void** state) {
+    struct Integer integer1 = {.value = 1};
+    GENC_LIST_ELEMENT_INIT(&integer1);
+    struct Integer integer2 = {.value = 2};
+    GENC_LIST_ELEMENT_INIT(&integer2);
+    GENC_LIST_ELEMENT_PREPEND(&integer1, &integer2);
+    assert_ptr_equal(GENC_LIST_ELEMENT_PREVIOUS(&integer1), &integer2);
+    assert_ptr_equal(GENC_LIST_ELEMENT_NEXT(&integer2), &integer1);
+}
+
+void GENC_LIST_ELEMENT_APPEND_test(void** state) {
+    struct Integer integer1 = {.value = 1};
+    GENC_LIST_ELEMENT_INIT(&integer1);
+    struct Integer integer2 = {.value = 2};
+    GENC_LIST_ELEMENT_INIT(&integer2);
+    GENC_LIST_ELEMENT_APPEND(&integer1, &integer2);
+    assert_ptr_equal(GENC_LIST_ELEMENT_NEXT(&integer1), &integer2);
+    assert_ptr_equal(GENC_LIST_ELEMENT_PREVIOUS(&integer2), &integer1);
+}
+
+void GENC_LIST_INSERT_BEFORE_test(void** state) {
+    struct Integer_List integerList;
+    GENC_LIST_INIT(&integerList);
+    struct Integer integer1 = {.value = 1};
+    GENC_LIST_ELEMENT_INIT(&integer1);
+    GENC_LIST_INSERT_BEFORE(&integerList, GENC_LIST_HEAD(&integerList), &integer1);
+    assert_int_equal(GENC_LIST_HEAD(&integerList)->value, 1);
+    assert_int_equal(GENC_LIST_TAIL(&integerList)->value, 1);
+    struct Integer integer2 = {.value = 2};
+    GENC_LIST_ELEMENT_INIT(&integer2);
+    GENC_LIST_INSERT_BEFORE(&integerList, GENC_LIST_HEAD(&integerList), &integer2);
+    assert_int_equal(GENC_LIST_HEAD(&integerList)->value, 2);
+    assert_int_equal(GENC_LIST_TAIL(&integerList)->value, 1);
+    struct Integer integer3 = {.value = 3};
+    GENC_LIST_ELEMENT_INIT(&integer3);
+    GENC_LIST_INSERT_BEFORE(&integerList, GENC_LIST_TAIL(&integerList), &integer3);
+    assert_int_equal(GENC_LIST_HEAD(&integerList)->value, 2);
+    assert_int_equal(GENC_LIST_TAIL(&integerList)->value, 1);
+}
+
+void GENC_LIST_INSERT_AFTER_test(void** state) {
+    struct Integer_List integerList;
+    GENC_LIST_INIT(&integerList);
+    struct Integer integer1 = {.value = 1};
+    GENC_LIST_ELEMENT_INIT(&integer1);
+    GENC_LIST_INSERT_AFTER(&integerList, GENC_LIST_TAIL(&integerList), &integer1);
+    assert_int_equal(GENC_LIST_HEAD(&integerList)->value, 1);
+    assert_int_equal(GENC_LIST_TAIL(&integerList)->value, 1);
+    struct Integer integer2 = {.value = 2};
+    GENC_LIST_ELEMENT_INIT(&integer2);
+    GENC_LIST_INSERT_AFTER(&integerList, GENC_LIST_TAIL(&integerList), &integer2);
+    assert_int_equal(GENC_LIST_TAIL(&integerList)->value, 2);
+    assert_int_equal(GENC_LIST_HEAD(&integerList)->value, 1);
+    struct Integer integer3 = {.value = 3};
+    GENC_LIST_ELEMENT_INIT(&integer3);
+    GENC_LIST_INSERT_AFTER(&integerList, GENC_LIST_HEAD(&integerList), &integer3);
+    assert_int_equal(GENC_LIST_TAIL(&integerList)->value, 2);
+    assert_int_equal(GENC_LIST_HEAD(&integerList)->value, 1);
+}
+
 void genc_List_test(void** state) {
-    struct Integer_List list;
-    GENC_LIST_INIT(&list);
-    struct Integer element1;
-    GENC_LIST_ELEMENT_INIT(&element1);
-    element1.value = 100;
-    struct Integer element2;
-    GENC_LIST_ELEMENT_INIT(&element2);
-    element2.value = 200;
-
-    GENC_LIST_INSERT_AFTER(&list, GENC_LIST_HEAD(&list), &element1);
-    GENC_LIST_INSERT_AFTER(&list, GENC_LIST_HEAD(&list), &element2);
-
-    GENC_LIST_FOR_EACH(&list, struct Integer, item)
-    {
-        printf("%d\n", item->value);
+    struct Integer_List integerList;
+/*
+    GENC_LIST_INIT(&integerList);
+    struct Integer* element = NULL;
+    int rawIntegerArray[] = {1, 2, 3, 4, 5};
+    int rawIntegerArraySize = sizeof(rawIntegerArray) / sizeof(int);
+    for(int index = 0; index < rawIntegerArraySize; ++index) {
+        element = malloc(sizeof(struct Integer));
+        GENC_LIST_ELEMENT_INIT(element);
+        element->value = rawIntegerArray[index];
+        GENC_LIST_INSERT_BEFORE(&integerList, GENC_LIST_HEAD(&integerList), element);
     }
-    printf("\n");
-    GENC_LIST_REVERSE_FOR_EACH(&list, struct Integer, item)
-    {
-        printf("%d\n", item->value);
-    }
-    printf("\n");
-    GENC_LIST_REMOVE(&list, GENC_LIST_HEAD(&list));
-    GENC_LIST_FOR_EACH(&list, struct Integer, item)
-    {
-        printf("%d\n", item->value);
-    }
-    printf("\n");
-    GENC_LIST_REMOVE(&list, GENC_LIST_HEAD(&list));
-    GENC_LIST_FOR_EACH(&list, struct Integer, item)
-    {
-        printf("%d\n", item->value);
-    }
-
-    GENC_LIST_INSERT_AFTER(&list, GENC_LIST_HEAD(&list), &element1);
-    GENC_LIST_INSERT_AFTER(&list, GENC_LIST_HEAD(&list), &element2);
-
-    struct Integer* element = GENC_LIST_HEAD(&list);
-
-    GENC_LIST_AFTER(&list, element, 1);
-    
-    printf("%d\n", element->value);
-
-    while(GENC_LIST_SIZE(&list) > 0)
-    {
-        GENC_LIST_REMOVE(&list, GENC_LIST_HEAD(&list));
-    }
-
-    printf("After destroy:\n");
-
-    GENC_LIST_FOR_EACH(&list, struct Integer, item)
-    {
-        printf("%d\n", item->value);
-    }
+    int* outputRawIntegerArray;
+    int outputRawIntegerArraySize;
+    Integer_List_toRawArray(&integerList, &outputRawIntegerArray, &outputRawIntegerArraySize);
+    assert_int_equal(rawIntegerArraySize, outputRawIntegerArraySize);
+    assert_memory_equal(rawIntegerArray, outputRawIntegerArray, outputRawIntegerArraySize);
+    free(outputRawIntegerArray);
+*/
 }
 
 int main() {
     const struct CMUnitTest tests[] = {
+        cmocka_unit_test(GENC_LIST_INIT_test),
+        cmocka_unit_test(GENC_LIST_ELEMENT_INIT_test),
+        cmocka_unit_test(GENC_LIST_ELEMENT_PREPEND_test),
+        cmocka_unit_test(GENC_LIST_ELEMENT_APPEND_test),
+        cmocka_unit_test(GENC_LIST_INSERT_BEFORE_test),
+        cmocka_unit_test(GENC_LIST_INSERT_AFTER_test),
         cmocka_unit_test(genc_List_test)
     };
 
