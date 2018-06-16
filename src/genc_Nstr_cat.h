5afbec8014125cc03de125fdde1a6ca1e725092c
Byeonggon Lee <gonny952@gmail.com>
2018-06-17 02:43:48 +0900

    genc_nStrCat -> genc_Nstr_cat

--- src/genc_Nstr_cat.h
+++ src/genc_Nstr_cat.h
@@ -1,5 +1,5 @@
 #include <string.h>
-static inline size_t genc_nStrCat(char* dest, size_t destCapacity, size_t* destSize, const char* src, size_t srcSize) {
+static inline size_t genc_Nstr_cat(char* dest, size_t destCapacity, size_t* destSize, const char* src, size_t srcSize) {
     if(*destSize + srcSize < destCapacity) {
         memcpy(dest + *destSize, src, srcSize);
         *destSize += srcSize;
