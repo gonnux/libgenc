550df881226654152420669dde62dc46e54d9600
Byeonggon Lee <gonny952@gmail.com>
2018-06-17 14:54:17 +0900

    Add tests to genc_Nstr_cat, fixed a bug

--- src/genc_Nstr_cat.h
+++ src/genc_Nstr_cat.h
@@ -1,6 +1,6 @@
 #include <string.h>
 static inline size_t genc_Nstr_cat(char* dest, size_t destCapacity, size_t* destSize, const char* src, size_t srcSize) {
-    if(*destSize + srcSize < destCapacity) {
+    if(*destSize + srcSize <= destCapacity) {
         memcpy(dest + *destSize, src, srcSize);
         *destSize += srcSize;
         return 0;
