6d7f5e280852769a4b9181baf58bc0a2dcda02da
Byeonggon Lee <gonny952@gmail.com>
2018-12-23 06:42:18 +0000

    Modified genc_HashMap_test

--- test/genc_HashMap_test.c
+++ test/genc_HashMap_test.c
@@ -8,44 +8,44 @@
 #include "../src/genc_String.h"
 
 int main() {
-    struct my_HashMap_element {
+    struct MyHashMapElement {
         int value;
-        GENC_HASH_MAP_ELEMENT(struct my_HashMap_element);
+        GENC_HASH_MAP_ELEMENT(struct MyHashMapElement);
     };
-    struct my_HashMap {
-        GENC_HASH_MAP(struct my_HashMap_element);
+    struct MyHashMap {
+        GENC_HASH_MAP(struct MyHashMapElement);
     };
-    struct my_HashMap HashMap;
+    struct MyHashMap HashMap;
     GENC_HASH_MAP_INIT(&HashMap, 10007);
 
-    struct my_HashMap_element* element;
+    struct MyHashMapElement* element;
 
-    struct my_HashMap_element* old_element;
+    struct MyHashMapElement* oldElement;
 
-    element = malloc(sizeof(struct my_HashMap_element));
+    element = malloc(sizeof(struct MyHashMapElement));
     GENC_HASH_MAP_ELEMENT_INIT(element);
     element->value = 100;
 
     GENC_HASH_MAP_ELEMENT_KEY(element) = "HELLO";
     GENC_HASH_MAP_ELEMENT_KEY_LENGTH(element) = 5;
 
-    GENC_HASH_MAP_SET(&HashMap, element, old_element);
+    GENC_HASH_MAP_SET(&HashMap, element, oldElement);
 
-    printf("%p\n", old_element);
+    printf("%p\n", oldElement);
 
-    element = malloc(sizeof(struct my_HashMap_element));
+    element = malloc(sizeof(struct MyHashMapElement));
     GENC_HASH_MAP_ELEMENT_INIT(element);
     element->value = 200;
 
     GENC_HASH_MAP_ELEMENT_KEY(element) = "HELLO";
     GENC_HASH_MAP_ELEMENT_KEY_LENGTH(element) = 5;
 
-    GENC_HASH_MAP_SET(&HashMap, element, old_element);
+    GENC_HASH_MAP_SET(&HashMap, element, oldElement);
 
-    printf("%p %d\n", old_element, old_element->value);
+    printf("%p %d\n", oldElement, oldElement->value);
 
-    struct my_HashMap_element* element_out;
-    GENC_HASH_MAP_GET(&HashMap, "HELLO", 5, element_out);
-    printf("%d\n", element_out->value);
+    struct MyHashMapElement* elementOut;
+    GENC_HASH_MAP_GET(&HashMap, "HELLO", 5, elementOut);
+    printf("%d\n", elementOut->value);
     return 0;
 }
