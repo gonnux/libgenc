7133b1a9052e5b8e837603d2e88079a7908f5929
Byeonggon Lee <gonny952@gmail.com>
2019-01-11 23:00:06 +0000

    Fixed a bug in GENC_TREE_NODE_ADD_CHILD()

--- test/genc_Tree_test.c
+++ test/genc_Tree_test.c
@@ -19,18 +19,29 @@ void genc_Tree_init3_test(void** state) {
 }
 
 void genc_Tree_test(void** state) {
-    int value = 100;
     struct node node;
-    node.value = value;
+    node.value = 100;
     GENC_TREE_NODE_INIT2(&node, 2);
     GENC_TREE_NODE_ADD_EMPTY_CHILD(&node);
+    assert_int_equal(GENC_TREE_NODE_CHILD_COUNT(&node), 1);
+}
+
+void genc_Tree_test2(void** state) {
+    struct node node;
+    node.value = 100;
+    GENC_TREE_NODE_INIT2(&node, 2);
+    struct node node2;
+    node2.value = 200;
+    GENC_TREE_NODE_ADD_CHILD(&node, &node2);
     assert_int_equal(GENC_TREE_NODE_CHILD_COUNT(&node), 1);
 }
 
 int main() {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(genc_Tree_init3_test),
-        cmocka_unit_test(genc_Tree_test)
+        cmocka_unit_test(genc_Tree_test),
+        cmocka_unit_test(genc_Tree_test2)
+
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
