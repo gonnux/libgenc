a97520aed3520cc7523907205bb449f2b951ea04
Byeonggon Lee <gonny952@gmail.com>
2019-01-13 07:37:53 +0000

    Add more tests

--- test/genc_Tree_test.c
+++ test/genc_Tree_test.c
@@ -14,12 +14,21 @@ struct node {
 void genc_Tree_test(void** state) {
     struct node node;
     node.value = 100;
-    GENC_TREE_NODE_INIT2(&node, 2);
-    struct node node2;
-    GENC_TREE_NODE_INIT2(&node2, 2);
-    node2.value = 200;
-    GENC_TREE_NODE_ADD_CHILD(&node, &node2);
-    assert_int_equal(GENC_TREE_NODE_CHILD_COUNT(&node), 1);
+    GENC_TREE_NODE_INIT2(&node, 10);
+    struct node* node2;
+    for(size_t index = 0; index != 10; ++index) {
+        node2 = malloc(sizeof(struct node));
+        GENC_TREE_NODE_INIT(node2);
+        node2->value = index * 100;
+        GENC_TREE_NODE_ADD_CHILD(&node, node2);
+    }
+    assert_int_equal(GENC_TREE_NODE_CHILD_COUNT(&node), 10);
+    for(size_t index = 0; index != 10; ++index) {
+        assert_ptr_equal(GENC_TREE_NODE_GET_CHILD(&node, index)->value, index * 100);
+        GENC_TREE_NODE_FREE(GENC_TREE_NODE_GET_CHILD(&node, index));
+        free(GENC_TREE_NODE_GET_CHILD(&node, index));
+    }
+    GENC_TREE_NODE_FREE(&node);
 }
 
 int main() {
