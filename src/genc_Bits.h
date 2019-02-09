487ef37f021c03987b7e492b1c8a3ea16872108d
Byeonggon Lee <gonny952@gmail.com>
2019-02-09 00:56:40 +0000

    Update genc_Bits.h

--- src/genc_Bits.h
+++ src/genc_Bits.h
@@ -1,10 +1,10 @@
 #ifndef _GENC_BITS_H
 #define _GENC_BITS_H
 
-#define GENC_BITS_ROTL(bits, count, size) \
+#define GENC_BITS_ROTL(bits, size, count) \
 (uint64_t)(((bits) << (count)) | ((bits) >> (size - (count))))
 
-#define GENC_BITS_ROTR(bits, count, size) \
+#define GENC_BITS_ROTR(bits, size, count) \
 (uint64_t)(((bits) << (size - (count))) | ((bits) >> (count)))
 
 #endif
