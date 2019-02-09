afa0521868ed938d394010eb5e0da203d751ab70
Byeonggon Lee <gonny952@gmail.com>
2019-02-09 03:08:29 +0000

    Update genc_Bytes.h

--- src/genc_Bytes.h
+++ src/genc_Bytes.h
@@ -1,12 +1,14 @@
 #ifndef _GENC_BYTES_H
 #define _GENC_BYTES_H
 
-#include <stdint.h>
+#include <string.h>
 
-#define GENC_BYTES_REVERSE(bytes, size, outBytes) { \
-    for(size_t index = 0; index != size; ++index) { \
+#define GENC_BYTES_REVERSE_COPY(bytes, size, outBytes) { \
+    for(size_t index = 0; index != size; ++index) \
         (outBytes)[index] = (bytes)[size - index - 1]; \
-    } \
 }
+
+#define GENC_BYTES_COPY(bytes, size, outBytes) \
+    mempcpy(outBytes, bytes, size);
 
 #endif
