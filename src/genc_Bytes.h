626c227b1c2349a83f97d83994dc479683e6c808
Byeonggon Lee <gonny952@gmail.com>
2019-02-09 01:59:10 +0000

    Update genc_Bytes.h

--- src/genc_Bytes.h
+++ src/genc_Bytes.h
@@ -1,16 +1,11 @@
 #ifndef _GENC_BYTES_H
 #define _GENC_BYTES_H
 
-#define <stdint.h>
+#include <stdint.h>
 
-#define GENC_BYTES_REVERSE(bytes, size) { \
-    uint8_t tmp; \
+#define GENC_BYTES_REVERSE(bytes, size, outBytes) { \
     for(size_t index = 0; index != size; ++index) { \
-        if(index == size - index - 1 || (bytes)[index] == (bytes)[size - index - 1]) \
-            break; \
-        tmp = (bytes)[index]; \
-	(bytes)[index] = (bytes)[size - index - 1]; \
-        (bytes)[size - index - 1] = (bytes)[index]; \
+        (outBytes)[index] = (bytes)[size - index - 1]; \
     } \
 }
 
