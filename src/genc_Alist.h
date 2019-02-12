0f098d1ef0479016d5fa95ca3fc272a4a392426d
Byeonggon Lee <gonny952@gmail.com>
2019-02-12 08:04:42 +0000

    Modified and renamed sources

--- src/genc_Alist.h
+++ src/genc_Alist.h
@@ -1,144 +1,148 @@
-#ifndef _GENC_ARRAY_LIST_H
-#define _GENC_ARRAY_LIST_H
+#ifndef _GENC_ALIST_H
+#define _GENC_ALIST_H
 
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define GENC_ARRAY_LIST(type) \
+#define GENC_ALIST(type) \
 struct { \
     type** elems; \
     size_t capacity; \
     size_t size; \
-} genc_ArrayList
+} genc_Alist
 
-#define GENC_ARRAY_LIST_ELEM_SIZE(arrayList) \
-sizeof(*(GENC_ARRAY_LIST_ELEMS(arrayList)))
+#define GENC_ALIST_ELEM_SIZE(arrayList) \
+sizeof(*(GENC_ALIST_ELEMS(arrayList)))
 
-#define GENC_ARRAY_LIST_CAPACITY(arrayList) \
-((arrayList)->genc_ArrayList.capacity)
+#define GENC_ALIST_CAPACITY(arrayList) \
+((arrayList)->genc_Alist.capacity)
 
-#define GENC_ARRAY_LIST_SIZE(arrayList) \
-((arrayList)->genc_ArrayList.size)
+#define GENC_ALIST_SIZE(arrayList) \
+((arrayList)->genc_Alist.size)
 
-#define GENC_ARRAY_LIST_ELEMS(arrayList) \
-((arrayList)->genc_ArrayList.elems)
+#define GENC_ALIST_ELEMS(arrayList) \
+((arrayList)->genc_Alist.elems)
 
-#define GENC_ARRAY_LIST_RAW_REALLOC(arrayList, newCapacity) do { \
-    GENC_ARRAY_LIST_CAPACITY(arrayList) = newCapacity; \
-    GENC_ARRAY_LIST_ELEMS(arrayList) = realloc(GENC_ARRAY_LIST_ELEMS(arrayList), \
-                                               GENC_ARRAY_LIST_CAPACITY(arrayList) \
-                                               * GENC_ARRAY_LIST_ELEM_SIZE(arrayList)); \
+#define GENC_ALIST_RAW_REALLOC(arrayList, newCapacity) do { \
+    GENC_ALIST_CAPACITY(arrayList) = newCapacity; \
+    GENC_ALIST_ELEMS(arrayList) = realloc(GENC_ALIST_ELEMS(arrayList), \
+                                               GENC_ALIST_CAPACITY(arrayList) \
+                                               * GENC_ALIST_ELEM_SIZE(arrayList)); \
 } while(0)
 
-#define GENC_ARRAY_LIST_SHRINK(arrayList) \
-GENC_ARRAY_LIST_RAW_REALLOC(arrayList, GENC_ARRAY_LIST_SIZE(arrayList)); \
+#define GENC_ALIST_SHRINK(arrayList) \
+GENC_ALIST_RAW_REALLOC(arrayList, GENC_ALIST_SIZE(arrayList)); \
 
-#define GENC_ARRAY_LIST_REALLOC(arrayList, newCapacity) do { \
+#define GENC_ALIST_REALLOC(arrayList, newCapacity) do { \
     if(newCapacity >= 0) { \
-        if(GENC_ARRAY_LIST_ELEMS(arrayList) != NULL) { \
-            if(newCapacity != GENC_ARRAY_LIST_CAPACITY(arrayList)) { \
-                if(newCapacity <= GENC_ARRAY_LIST_SIZE(arrayList)) { \
-                    GENC_ARRAY_LIST_SHRINK(arrayList); \
+        if(GENC_ALIST_ELEMS(arrayList) != NULL) { \
+            if(newCapacity != GENC_ALIST_CAPACITY(arrayList)) { \
+                if(newCapacity <= GENC_ALIST_SIZE(arrayList)) { \
+                    GENC_ALIST_SHRINK(arrayList); \
                 } else { \
-                    GENC_ARRAY_LIST_RAW_REALLOC(arrayList, newCapacity); \
+                    GENC_ALIST_RAW_REALLOC(arrayList, newCapacity); \
                 } \
             } \
         } else { \
-            GENC_ARRAY_LIST_ELEMS(arrayList) = calloc(newCapacity, GENC_ARRAY_LIST_ELEM_SIZE(arrayList)); \
-            GENC_ARRAY_LIST_CAPACITY(arrayList) = newCapacity; \
+            GENC_ALIST_ELEMS(arrayList) = calloc(newCapacity, GENC_ALIST_ELEM_SIZE(arrayList)); \
+            GENC_ALIST_CAPACITY(arrayList) = newCapacity; \
         } \
     } \
 } while(0)
 
-#define GENC_ARRAY_LIST_REALLOC2(arrayList) do { \
-    if(GENC_ARRAY_LIST_SIZE(arrayList) == GENC_ARRAY_LIST_CAPACITY(arrayList)) \
-        GENC_ARRAY_LIST_RAW_REALLOC(arrayList, (GENC_ARRAY_LIST_CAPACITY(arrayList) + 1) * 2); \
+#define GENC_ALIST_REALLOC2(arrayList) do { \
+    if(GENC_ALIST_SIZE(arrayList) == GENC_ALIST_CAPACITY(arrayList)) \
+        GENC_ALIST_RAW_REALLOC(arrayList, (GENC_ALIST_CAPACITY(arrayList) + 1) * 2); \
 } while(0)
 
-#define GENC_ARRAY_LIST_RAW_MOVE(arrayList, beginIndex, endIndex, moveCount) do { \
+#define GENC_ALIST_RAW_MOVE(arrayList, beginIndex, endIndex, moveCount) do { \
     memmove( \
-        GENC_ARRAY_LIST_ELEMS(arrayList) + endIndex, \
-        GENC_ARRAY_LIST_ELEMS(arrayList) + beginIndex, \
-        (moveCount) * GENC_ARRAY_LIST_ELEM_SIZE(arrayList)); \
+        GENC_ALIST_ELEMS(arrayList) + endIndex, \
+        GENC_ALIST_ELEMS(arrayList) + beginIndex, \
+        (moveCount) * GENC_ALIST_ELEM_SIZE(arrayList)); \
 } while(0)
 
-#define GENC_ARRAY_LIST_INIT(arrayList) do { \
-    GENC_ARRAY_LIST_ELEMS(arrayList) = NULL; \
-    GENC_ARRAY_LIST_CAPACITY(arrayList) = 0; \
-    GENC_ARRAY_LIST_SIZE(arrayList) = 0; \
+#define GENC_ALIST_INIT(arrayList) do { \
+    GENC_ALIST_ELEMS(arrayList) = NULL; \
+    GENC_ALIST_CAPACITY(arrayList) = 0; \
+    GENC_ALIST_SIZE(arrayList) = 0; \
 } while(0)
 
-#define GENC_ARRAY_LIST_ZERO_ELEMS(arrayList, beginIndex, endIndex) do { \
-    if(GENC_ARRAY_LIST_SIZE(arrayList) > 0 && endIndex - beginIndex >= 0) \
-        memset(GENC_ARRAY_LIST_ELEMS(arrayList) + beginIndex, 0, (endIndex - beginIndex + 1) * GENC_ARRAY_LIST_ELEM_SIZE(arrayList)); \
+#define GENC_ALIST_ZERO_ELEMS(arrayList, beginIndex, endIndex) do { \
+    if(GENC_ALIST_SIZE(arrayList) > 0 && endIndex - beginIndex >= 0) \
+        memset(GENC_ALIST_ELEMS(arrayList) + beginIndex, 0, (endIndex - beginIndex + 1) * GENC_ALIST_ELEM_SIZE(arrayList)); \
 } while(0)
 
-#define GENC_ARRAY_LIST_INIT2(arrayList, initialCapacity) do { \
-    GENC_ARRAY_LIST_INIT(arrayList); \
-    GENC_ARRAY_LIST_REALLOC(arrayList, initialCapacity); \
+#define GENC_ALIST_INIT2(arrayList, initialCapacity) do { \
+    GENC_ALIST_INIT(arrayList); \
+    GENC_ALIST_REALLOC(arrayList, initialCapacity); \
 } while(0)
 
-#define GENC_ARRAY_LIST_FREE(arrayList) do { \
-    free(GENC_ARRAY_LIST_ELEMS(arrayList)); \
-    GENC_ARRAY_LIST_INIT(arrayList); \
+#define GENC_ALIST_FREE(arrayList) do { \
+    free(GENC_ALIST_ELEMS(arrayList)); \
+    GENC_ALIST_INIT(arrayList); \
 } while(0)
 
-#define GENC_ARRAY_LIST_GET(arrayList, index) \
-(GENC_ARRAY_LIST_ELEMS(arrayList)[index])
+#define GENC_ALIST_RAW_GET(arrayList, index) \
+(GENC_ALIST_ELEMS(arrayList)[index])
 
-#define GENC_ARRAY_LIST_GET2(arrayList, index, elem) \
-*elem = GENC_ARRAY_LIST_GET(arrayList, index)
-
-#define GENC_ARRAY_LIST_RAW_SET(arrayList, index, elem) \
-GENC_ARRAY_LIST_ELEMS(arrayList)[index] = elem
-
-#define GENC_ARRAY_LIST_SET(arrayList, index, elem) do { \
-    if(index < GENC_ARRAY_LIST_SIZE(arrayList)) \
-        GENC_ARRAY_LIST_RAW_SET(arrayList, index, elem); \
+#define GENC_ALIST_GET(arrayList, index, elem) do{ \
+    if(index < GENC_ALIST_SIZE(arrayList)) \
+        *(elem) = GENC_ALIST_RAW_GET(arrayList, index); \
+    else \
+        *(elem) = NULL; \
 } while(0)
 
-#define GENC_ARRAY_LIST_PEEK(arrayList) \
-GENC_ARRAY_LIST_GET(arrayList, GENC_ARRAY_LIST_SIZE(arrayList) - 1)
+#define GENC_ALIST_RAW_SET(arrayList, index, elem) \
+GENC_ALIST_ELEMS(arrayList)[index] = elem
 
-#define GENC_ARRAY_LIST_PEEK2(arrayList, elem) \
-GENC_ARRAY_LIST_GET2(arrayList, GENC_ARRAY_LIST_SIZE(arrayList) - 1, elem)
+#define GENC_ALIST_SET(arrayList, index, elem) do { \
+    if(index < GENC_ALIST_SIZE(arrayList)) \
+        GENC_ALIST_RAW_SET(arrayList, index, elem); \
+} while(0)
 
-#define GENC_ARRAY_LIST_INSERT(arrayList, index, elem) do { \
-    if(index > GENC_ARRAY_LIST_SIZE(arrayList)) \
+#define GENC_ALIST_RAW_PEEK(arrayList) \
+GENC_ALIST_GET(arrayList, GENC_ALIST_SIZE(arrayList) - 1)
+
+#define GENC_ALIST_PEEK(arrayList, elem)  \
+GENC_ALIST_GET(arrayList, GENC_ALIST_SIZE(arrayList) - 1, elem)
+
+#define GENC_ALIST_INSERT(arrayList, index, elem) do { \
+    if(index > GENC_ALIST_SIZE(arrayList)) \
         break; \
-    GENC_ARRAY_LIST_REALLOC2(arrayList); \
-    if(index < GENC_ARRAY_LIST_SIZE(arrayList)) \
-        GENC_ARRAY_LIST_RAW_MOVE(arrayList, index, index + 1, GENC_ARRAY_LIST_SIZE(arrayList) - index); \
-    GENC_ARRAY_LIST_RAW_SET(arrayList, index, elem); \
-    ++GENC_ARRAY_LIST_SIZE(arrayList); \
+    GENC_ALIST_REALLOC2(arrayList); \
+    if(index < GENC_ALIST_SIZE(arrayList)) \
+        GENC_ALIST_RAW_MOVE(arrayList, index, index + 1, GENC_ALIST_SIZE(arrayList) - index); \
+    GENC_ALIST_RAW_SET(arrayList, index, elem); \
+    ++GENC_ALIST_SIZE(arrayList); \
 } while(0)
 
-#define GENC_ARRAY_LIST_PUSH(arrayList, elem) \
-GENC_ARRAY_LIST_INSERT(arrayList, GENC_ARRAY_LIST_SIZE(arrayList), elem)
+#define GENC_ALIST_PUSH(arrayList, elem) \
+GENC_ALIST_INSERT(arrayList, GENC_ALIST_SIZE(arrayList), elem)
 
-#define GENC_ARRAY_LIST_REMOVE(arrayList, index, elem) do { \
-    if(index > GENC_ARRAY_LIST_SIZE(arrayList) - 1) \
+#define GENC_ALIST_REMOVE(arrayList, index, elem) do { \
+    if(index > GENC_ALIST_SIZE(arrayList) - 1) \
         break; \
-    *elem = GENC_ARRAY_LIST_GET(arrayList, index); \
-    if(index < GENC_ARRAY_LIST_SIZE(arrayList) - 1) \
-        GENC_ARRAY_LIST_RAW_MOVE(arrayList, index + 1, index, GENC_ARRAY_LIST_SIZE(arrayList) - index - 1); \
-    --GENC_ARRAY_LIST_SIZE(arrayList); \
+    *elem = GENC_ALIST_RAW_GET(arrayList, index); \
+    if(index < GENC_ALIST_SIZE(arrayList) - 1) \
+        GENC_ALIST_RAW_MOVE(arrayList, index + 1, index, GENC_ALIST_SIZE(arrayList) - index - 1); \
+    --GENC_ALIST_SIZE(arrayList); \
 } while(0)
 
-#define GENC_ARRAY_LIST_POP(arrayList, elem) \
-GENC_ARRAY_LIST_REMOVE(arrayList, GENC_ARRAY_LIST_SIZE(arrayList) - 1, elem)
+#define GENC_ALIST_POP(arrayList, elem) \
+GENC_ALIST_REMOVE(arrayList, GENC_ALIST_SIZE(arrayList) - 1, elem)
 
-#define GENC_ARRAY_LIST_FOR_EACH(arrayList, index) \
-for(size_t index = 0; index != GENC_ARRAY_LIST_SIZE(arrayList); ++index)
+#define GENC_ALIST_FOR_EACH(arrayList, index) \
+for(size_t index = 0; index != GENC_ALIST_SIZE(arrayList); ++index)
 
-#define GENC_ARRAY_LIST_REVERSE_FOR_EACH(arrayList, index) \
-for(size_t index = GENC_ARRAY_LIST_SIZE(arrayList) - 1; index != -1; --index)
+#define GENC_ALIST_REVERSE_FOR_EACH(arrayList, index) \
+for(size_t index = GENC_ALIST_SIZE(arrayList) - 1; index != -1; --index)
 
-#define GENC_ARRAY_LIST_SUBSET_FOR_EACH(arrayList, index, start, end) \
+#define GENC_ALIST_SUBSET_FOR_EACH(arrayList, index, start, end) \
 for(size_t index = start; index != end; ++index)
 
-#define GENC_ARRAY_LIST_REVERSE_SUBSET_FOR_EACH(arrayList, index, end, start) \
+#define GENC_ALIST_REVERSE_SUBSET_FOR_EACH(arrayList, index, end, start) \
 for(size_t index = end; index != start; --index)
 
 #endif
